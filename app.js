f3c29e884f327b5faf15c940f46517ca33122e91
Richard Deane <titch@hotmail.co.uk>
2017-12-13 21:38:45 +0000

    Checkpoint 8 ToDo: Completed

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
 function onReady() {
+
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-
-    let title = newToDoText.value;
-
-    let newLi = document.createElement('li');
-
-    let checkbox = document.createElement('input');
-
-    checkbox.type = "checkbox";
-
-    newLi.textContent = title;
-
-    newLi.appendChild(checkbox);
-
-    toDoList.appendChild(newLi);
+  function createNewToDo() {
+    if (!newToDoText.value) { return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
     newToDoText.value = '';
 
+    renderTheUI();
+  }
 
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    const title = document.createElement('span');
+    newLi.textContent = toDo.title;
+
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
 
   });
+
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
+  });
+
+  renderTheUI();
 }
+
+
 
 window.onload = function() {
    onReady();
